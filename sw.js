d5d6398ae1a47630aac0ae4ef4ccbb54632e80a3
kaspe1985 <christian.jeovanny28@gmail.com>
2026-09-13 11:41:07 -0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'tiki-taco-pos-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
